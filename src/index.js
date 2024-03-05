8264cf7a339afe372bd9b22bb3ea731ec137ebd0
Toon Baeyens <tbaeyens@think-cell.com>
2024-03-05 15:05:55 +0100

    more in the toc

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import Reveal from 'reveal.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
+import cpp from 'highlight.js/lib/languages/cpp';
 
-let deck = new Reveal({})
+let deck = new Reveal({
+    plugins: [RevealHighlight],
+    highlight: {
+        beforeHighlight: hljs => hljs.registerLanguage("c++", cpp)
+    },
+    slideNumber: 'c/t',
+    margin: 0,
+    width: 1920,
+    height: 1080,
+    center: false,
+    hash: true,
+});
 deck.initialize();
