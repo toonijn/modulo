612b5c2853f4c6080c698ced88c162cb6883e6d2
Toon Baeyens <toon.baeyens@gmail.com>
2024-04-02 14:27:41 +0200

    tweaks

--- src/code/ct_operators.cpp
+++ src/code/ct_operators.cpp
@@ -1,22 +1,27 @@
 #include "mod/value_type.h"
-#include <complex>
 
 template<std::uint64_t modulus_>
 struct Modulo {
     static constexpr ValueType<modulus_> modulus = modulus_;
     ValueType<modulus_> value;
 
-    // ... Constructors
-
     template<typename Rhs>
     Modulo<modulus_> operator+=(Rhs&& rhs) {
-        value += Modulo(std::forward<Rhs>(rhs)).value;
+        if constexpr(std::is_same<std::decay_t<Rhs>, Modulo<modulus_>>::value) {
+            value += rhs.value;
+        } else {
+            value += Modulo<modulus_>(std::forward<Rhs>(rhs)).value;
+        }
         if(value >= modulus) value -= modulus;
         return *this;
     }
 
     template<typename Lhs, typename Rhs>
     friend Modulo<modulus_> operator+(Lhs&& lhs, Rhs&& rhs) {
-        return Modulo<modulus_>(std::forward<Lhs>(lhs)) += std::forward<Rhs>(rhs);
+        if constexpr(std::is_same<decltype(lhs), Modulo<modulus_>&&>::value) {
+            return lhs += std::forward<Rhs>(rhs);
+        } else {
+            return Modulo<modulus_>(std::forward<Lhs>(lhs)) += std::forward<Rhs>(rhs);
+        }
     }
 };
